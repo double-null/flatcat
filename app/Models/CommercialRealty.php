2d14a883f47696aa4b0f58aefab657a64ae785f7
Double Null <double-null@yandex.ua>
2020-07-19 21:38:41 +0300

    RESTRUCT REALTY

---
+++ app/Models/CommercialRealty.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DoubleTrouble
+ * Date: 18.07.2020
+ * Time: 23:58
+ */
+
+namespace App\Models;
+
+
+class CommercialRealty
+{
+
+}
---
+++ app/Models/Realty.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DoubleTrouble
+ * Date: 19.07.2020
+ * Time: 16:13
+ */
+
+namespace App\Models;
+
+
+class Realty
+{
+
+}
