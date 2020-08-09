deae25e2427ccfb11a34a6bc85f5dd3008818466
Double Null <double-null@yandex.ua>
2020-08-09 14:26:57 +0300

    REVIEW RESTRUCT

---
+++ app/Controllers/UserDescController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DoubleTrouble
+ * Date: 08.08.2020
+ * Time: 10:30
+ */
+
+namespace App\Controllers;
+
+
+class UserDescController
+{
+
+}
---
+++ app/Models/UserDesc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DoubleTrouble
+ * Date: 08.08.2020
+ * Time: 15:55
+ */
+
+namespace App\Models;
+
+
+class UserDesc
+{
+
+}
